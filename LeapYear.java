abec5a8232ba642c46c516f4bffc896315457512
Carlos Rivera <crivera@live.esu.edu>
2026-03-25 09:34:24 -0400

    Simplified logic in leapyear

--- LeapYear.java
+++ LeapYear.java
@@ -10,10 +10,12 @@ public class LeapYear
         System.out.print("Please enter a year:");
         int year = keyboard.nextInt();
         boolean leap = false;
-        
+
         // use boolean operators to simplify the leap year determination
-		
-        
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            leap = true;
+        }
+
         if (leap) {
             System.out.println(year + " is a leap year.");
         } else {
